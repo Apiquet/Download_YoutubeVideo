943892a1ff17895b1979177cb59e73d577f6d417
Anthony Piquet <anthony.piquet2@gmail.com>
2018-05-28 19:17:17 -0700

    Simple form creation

--- Download_YoutubeVideo/Download_YoutubeVideo/Form1.Designer.cs
+++ Download_YoutubeVideo/Download_YoutubeVideo/Form1.Designer.cs
@@ -28,13 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Download = new System.Windows.Forms.Button();
+            this.Address = new System.Windows.Forms.Label();
+            this.textAddress = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Download
+            // 
+            this.Download.Location = new System.Drawing.Point(98, 86);
+            this.Download.Name = "Download";
+            this.Download.Size = new System.Drawing.Size(75, 23);
+            this.Download.TabIndex = 0;
+            this.Download.Text = "Download";
+            this.Download.UseVisualStyleBackColor = true;
+            // 
+            // Address
+            // 
+            this.Address.AutoSize = true;
+            this.Address.Location = new System.Drawing.Point(44, 54);
+            this.Address.Name = "Address";
+            this.Address.Size = new System.Drawing.Size(48, 13);
+            this.Address.TabIndex = 1;
+            this.Address.Text = "Address:";
+            // 
+            // textAddress
+            // 
+            this.textAddress.Location = new System.Drawing.Point(98, 51);
+            this.textAddress.Name = "textAddress";
+            this.textAddress.Size = new System.Drawing.Size(335, 20);
+            this.textAddress.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(492, 162);
+            this.Controls.Add(this.textAddress);
+            this.Controls.Add(this.Address);
+            this.Controls.Add(this.Download);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Download;
+        private System.Windows.Forms.Label Address;
+        private System.Windows.Forms.TextBox textAddress;
     }
 }
 
